75c99807f8a1cd32accc3684579f79f5273608b0
Mithun Mohandas <160577864+mithun-snive@users.noreply.github.com>
2024-04-21 17:52:47 +0530

    logo bug fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
 
             <section className="max-w-4xl ms-4 p-4 flex justify-between items-center">
                 <div className="flex sm:m-10 justify-center w-full sm:justify-start">
-                    <img className='w-[250px] pt-10 sm:pt-0' src="/images/logo_full.png" alt="Logo" />
+                    <img className='w-[250px] mt-10 sm:pt-0' src="/images/logo_full.png" alt="Logo" />
                 </div>
                 
             </section>
